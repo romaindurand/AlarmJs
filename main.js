fb0649cdfd2ca9397490f9fa6904504ea04da6b3
Author Name <sirius2006@gmail.com>
2015-07-18 21:08:23 +0200

    Added time fields + keyboard control

--- main.js
+++ main.js
@@ -5,15 +5,50 @@ var win = require('nw.gui').Window.get();
 $(button_test).click(ring);
 $(button_set).click(setTimer);
 $(button_settings).click(toggleDevTools);
+$([input_hour, input_minutes]).keydown(handleKeyboard);
+
+function handleKeyboard(e) {
+  var val = +$(e.target).val();
+  var input = e.target;
+  switch(e.keyCode) {
+    case 38: //up
+      e.preventDefault();
+      val++;
+    break;
+
+    case 40: //up
+      e.preventDefault();
+      val--;
+    break;
+  }
+  setInputVal(input, val);
+}
+
+function setInputVal(input, val) {
+  var checkedVal = checkInputVal(input, val);
+  if(checkedVal < 10) checkedVal = "0" + checkedVal;
+  $(input).val(checkedVal);
+}
+
+function checkInputVal(input, val) {
+  if(val < 0) {
+    return (input.id === "input_hour")? 23 : 59;
+  }
+
+  if(val > 23 && input.id === "input_hour") return 0;
+  if(val > 59 && input.id === "input_minutes")  return 0;
+
+  return val;
+}
 
 function ring(url) {
   alarm_container.src = url || $(input_url).val();
 }
 
-function setTimer () {
+function setTimer() {
   timers.push({
-    hour : 6,
-    minutes : 20,
+    hour : +$(input_hour).val(),
+    minutes : +$(input_minutes).val(),
     url : $(input_url).val(),
     hasRang : false
   });
